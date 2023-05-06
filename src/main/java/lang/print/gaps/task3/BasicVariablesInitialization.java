075342455a5499e3c5dd5b4799427e2f8d158fdd
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2023-05-06 22:11:26 +0200

    Finished.

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,6 +2,14 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int first;
+        int second;
+        int third;
+        first = 1;
+        System.out.println(first);
+        second = 10;
+        System.out.println(second);
+        third = 100;
+        System.out.println(third);
     }
 }
